1eee3d15edd5bc4ad92dbcf512f10bff6e726016
sittihawa184 <79967438+sittihawa184@users.noreply.github.com>
2021-03-05 20:47:10 +0800

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -6,16 +6,48 @@ import java.util.Scanner;
  *
  * @author Sugiarto Cokrowibowo
  */
-public class Main {
+Public Class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
         //TULISKAN KODEMU DIBAWAH INI
-        
-        
-        
-    }
 
+     package belajar.java;
+
+import java.util.Scanner;
+
+public class HawaCode1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>=1 && n<=12){
+            if (n == 1)  {
+                System.out.println("Januari\nFebruari\nMaret\nApril");
+            }else if(n==2){
+                System.out.println("Mei\nJuni\nJuli\nAgustus");
+            }else if (n==3){
+                System.out.println("September\nOktober\nNovember\nDesember");
+            }else if (n==4){
+                System.out.println("Februari\nMaret\nApril\nMei");
+            }else if (n==5){
+                System.out.println("Juni\nJuli\nAgustus\nSeptember");
+            }else if (n==6){
+                System.out.println("Oktober\nNovember\nDesember\nJanuari");
+            }else if (n==7){
+                System.out.println("November\nDesember\nJanuari\nFebruari");
+            }else if (n==8){
+                System.out.println("Maret\nApril\nMei\nJuni");
+            }else if (n==9){
+                System.out.println("Juli\nAgustus\nSeptember\nOktober");
+            }else if (n==10){
+                System.out.println("Mei\nJuni\nJuli\nAgustus");
+            }else if (n==11){
+                System.out.println("September\nOktober\nNovember\nDesember");
+            }else if (n==12){
+                System.out.println("Januari\nFebruari\nMaret\nApril");
+            }
+    }
 }
+
